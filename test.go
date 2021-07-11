87a2f6ba9356aa9866e4dfaa6ef7d06d7beb4fbf
osdifera <neezcafe@gmail.com>
2021-07-11 12:28:51 -0500

    chore: adding marshaling/unmarshaling

--- test.go
+++ test.go
@@ -2,23 +2,50 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/osdifera/gRPC/proto"
+	"github.com/osdifera/gRPC/pbmodels"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
 )
 
 func main(){
-	doMessage()
+	ms:=doPerson()
+	msAsString:=toJson(ms)
+	fmt.Printf("%s \n",msAsString)
+
+	pb:=&pbmodels.Person{}
+	fromJson(msAsString,pb)
+	fmt.Println(pb)
 }
 
-func doMessage(){
-	person := &proto.Person{
+func toJson(pb proto.Message) []byte {
+
+	out, err := protojson.Marshal(pb)
+	if err != nil {
+		log.Fatal("The message could not being marshaled", err)
+		return nil
+	}
+	return out
+}
+
+func fromJson(str []byte, pb proto.Message) {
+	err := protojson.Unmarshal(str, pb)
+	if (err != nil){
+		log.Fatal("Could not unmarshal ", err)
+	}
+}
+
+func doPerson() *pbmodels.Person{
+	person := &pbmodels.Person{
 		Name: "Hiro",
 		Lastname: "Watanabe",
 		Age: 15,
 	}
 
 	fmt.Println(person)
-
 	person.Age = 25
 	fmt.Println(person)
+
+	return person
 }
